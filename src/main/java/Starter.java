cdb0b4194113352cb183cd5b7d0fca45139fd02b
Viduravi Adithya <wviduraviadithya@gmail.com>
2025-12-03 12:51:55 +0530

    Login page UI

---
+++ src/main/java/Starter.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Starter extends Application {
+    @Override
+    public void start(Stage stage){
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ui/login_page.fxml")))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Starter.launch();
+    }
+}
